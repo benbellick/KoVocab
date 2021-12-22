d7f7d75c01fe6a33f76907f140c6dc1c7c28a69e
Ben Bellick <benbellick@protonmail.com>
2021-12-21 19:08:34 -0500

    Add word punctuation stripping and save date as datetime object

--- WordExtractor.py
+++ WordExtractor.py
@@ -1,4 +1,6 @@
 import sqlite3
+import re
+from datetime import datetime
 
 class WordExtractor:
     """This is a simple class to extract vocab word information from a Kobo eReader"""
@@ -13,11 +15,17 @@ class WordExtractor:
 
     def get_all_word_data(self):
         sqlStatement = "SELECT * FROM WordList;"
-        data_zipper = lambda word_data : {
-            "word": word_data[0],
+        return map(self._data_zipper, self.con.execute(sqlStatement))
+
+
+    def _data_zipper(self, word_data):
+        reg = re.compile("[^a-zA-Z]$")
+        #We strip last non-alphanumeric char (e.g. ;,') and lowercase
+        return {
+            "word": reg.sub('', word_data[0]).lower(),
             "location": word_data[1],
             "language": word_data[2],
-            "date": word_data[3]
+            "date": datetime.strptime(word_data[3], '%Y-%m-%dT%H:%M:%SZ')
         }
-        return map(data_zipper, self.con.execute(sqlStatement))
+        
 
